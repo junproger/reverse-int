8df99fa66677dcc70215ebfc1cc1f53862f9a321
Junior Programmer <progphyton@gmail.com>
2022-01-05 18:09:18 +0300

    feat: the task reverse int is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+	let input = n;
+	let number = Math.abs(input);
+	let string = number.toString();
+	let transform = 0;
+	let output = 0;
+	// console.log(string);
+		for (let i = 1; i <= string.length; i++) {
+			transform = (number % (10 ** i));
+			transform = (Math.trunc((transform * 10) / (10 ** i))) * (10 ** (string.length - i));
+			output += transform;
+		}
+    // console.log(output);
+	return output;
 }
